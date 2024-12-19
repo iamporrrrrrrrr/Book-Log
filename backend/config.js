dd26f0904d2e07d43ccfe3fa6b44c7c0304a8ca6
Theeraphat <theeraphat.tr@gmail.com>
2024-12-19 21:59:54 +0700

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT =5555;
+
+export const mongoDBURL = 'mongodb+srv://USERNAME_HERE:<PUT_PASSWORD_HERE>.tznow.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
+
